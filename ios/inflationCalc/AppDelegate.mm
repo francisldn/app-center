9636815dc420fb66b530c4046a4479247036974d
Francis_ldn <fnhh04@gmail.com>
2023-11-02 23:39:14 +0000

    add configuration

--- ios/inflationCalc/AppDelegate.mm
+++ ios/inflationCalc/AppDelegate.mm
@@ -3,6 +3,10 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTLinkingManager.h>
 
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -12,7 +16,11 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
+  
+  [AppCenterReactNative register];
+  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
+  
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
